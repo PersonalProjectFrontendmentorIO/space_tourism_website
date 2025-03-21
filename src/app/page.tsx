b6d4cc3d58deb8108cb21164fac59f26524f36be
Aldi-H <aldihimawan88@gmail.com>
2025-03-21 22:38:45 +0700

    feat: update background image handling for responsive design in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('../assets/images/Home/MobileHomeBackground.svg')] lg:bg-[url('../assets/images/Home/TabletHomeBackground.svg')] xl:bg-[url('../assets/images/Home/DesktopHomeBackground.svg')] bg-cover">
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('../assets/images/Home/MobileHomeBackground.svg')] md:bg-[url('../assets/images/Home/TabletHomeBackground.svg')] xl:bg-[url('../assets/images/Home/DesktopHomeBackground.svg')] bg-cover">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 				<h1 className="text-space-preset-2-size leading-space-preset-2 tracking-space-normal font-barlow-condensed">
 					Hello World 0 1 2 3
