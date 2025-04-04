29ed5cc821133919a72ceb97f6e282176766ff68
Aldi-H <aldihimawan88@gmail.com>
2025-04-04 14:42:41 +0700

    feat: enhance Subnavbar with active link highlighting and add getCurrentDestination utility

--- src/components/ui/subnavbar.tsx
+++ src/components/ui/subnavbar.tsx
@@ -16,13 +16,20 @@ const Subnavbar = () => {
 						return (
 							<Link
 								key={item.id}
-                href={item.path}
+								href={item.path}
 								className="font-barlow-condensed tracking-space-wider flex cursor-pointer h-full items-center justify-center mx-4"
-                passHref prefetch
+								passHref
+								prefetch
 							>
 								<div className="flex relative h-full xl:text-space-preset-9-size items-center justify-center">
 									<div className="flex gap-x-8 h-full items-center justify-center">
-										<div className="mb-3.5 text-center">{item.label}</div>
+										<div
+											className={`mb-3.5 text-center ${
+												currentPath === item.path && `text-space-white`
+											} text-space-blue-300`}
+										>
+											{item.label}
+										</div>
 									</div>
 									{currentPath === item.path && (
 										<div className="absolute bottom-0 w-full h-[3px] bg-space-white rounded-full"></div>
--- src/lib/subnavItems.ts
+++ src/lib/subnavItems.ts
@@ -1,28 +1,35 @@
+import { DestinationKey } from "./destinationImages";
+
 interface SubnavItem {
-  id: string;
-  label: string;
-  path: string;
+	id: string;
+	label: string;
+	path: string;
 }
 
 export const subnavItems: SubnavItem[] = [
-  {
-    id: "MOON",
-    label: "MOON",
-    path: "/destination/moon",
-  },
-  {
-    id: "MARS",
-    label: "MARS",
-    path: "/destination/mars",
-  },
-  {
-    id: "EUROPA",
-    label: "EUROPA",
-    path: "/destination/europa",
-  },
-  {
-    id: "TITAN",
-    label: "TITAN",
-    path: "/destination/titan",
-  },
-]
+	{
+		id: "MOON",
+		label: "MOON",
+		path: "/destination/moon",
+	},
+	{
+		id: "MARS",
+		label: "MARS",
+		path: "/destination/mars",
+	},
+	{
+		id: "EUROPA",
+		label: "EUROPA",
+		path: "/destination/europa",
+	},
+	{
+		id: "TITAN",
+		label: "TITAN",
+		path: "/destination/titan",
+	},
+];
+
+export const getCurrentDestination = (pathName: string): DestinationKey => {
+	const parts = pathName.split("/");
+	return (parts[parts.length - 1] as DestinationKey) || "moon";
+};
