6427a034185da9fe616ae2caf9024136c1b833ec
Aldi-H <aldihimawan88@gmail.com>
2025-04-14 12:30:45 +0700

    feat: add CrewLayout and CrewPage components for crew section

---
+++ src/app/crew/page.tsx
@@ -0,0 +1,13 @@
+import CrewMember from "@/components/crew/crewMember";
+
+const CrewPage = () => {
+	return (
+    <div className="bg-[url('../assets/images/crew/DesktopCrewBackground.svg')] bg-cover bg-center max-h-full h-full">
+      <div>
+        <CrewMember />
+      </div>
+    </div>
+	);
+};
+
+export default CrewPage;
