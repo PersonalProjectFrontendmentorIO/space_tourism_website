ffb2ffd067a5b6005819b131e11244f9704ae8c1
Aldi-H <aldihimawan88@gmail.com>
2025-04-18 13:39:06 +0700

    feat: add TechnologyLayout and TechnologyPage components for technology section

---
+++ src/app/technology/page.tsx
@@ -0,0 +1,11 @@
+import SpaceTechnology from "@/components/technology/spaceTechnology";
+
+const TechnologyPage = () => {
+	return (
+		<div className="bg-[url('../assets/images/technology/DesktopTechnologyBackground.svg')] bg-cover bg-center min-h-screen h-full">
+			<SpaceTechnology />
+		</div>
+	);
+};
+
+export default TechnologyPage;
